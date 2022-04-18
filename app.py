1135b4b57c3a1977ef379e30a0ff3c33375f8737
nicolebplatt <nicoleplatt@Nicoles-MacBook-Air.local>
2022-04-18 16:11:33 -0400

    Added flask routes

--- app.py
+++ app.py
@@ -1,11 +1,81 @@
-# import dependency
-from flask import Flask
-# create new flask app instance
+# import dependencies
+from flask import Flask, jsonify
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# setup database (create engine & access database)
+engine = create_engine("sqlite:///hawaii.sqlite")
+# reflect database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# create new flask instance called 'app'
 app = Flask(__name__)
-# create flask route
+# create flask home route
 @app.route("/")
-def hello_world():
-    return 'Hello world'
-# run flask app -- had to run in command line according to CS YouTube video
+# create function to show/return other routes
+# not working the way the module had it
+# I needed to store my text as an f-string and add a line break in HTML (<br)
+def welcome():
+    text = (f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>" 
+        f"/api/v1.0/temp/start/end")
+    return (text)
+
+# create precipitation route
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp)\
+        .filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+# create station route
+@app.route('/api/v1.0/stations')
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+# create temperature route
+@app.route('/api/v1.0/tobs')
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).filter(Measurement.station=='USC00519281')\
+        .filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+# create statistics route
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.max(Measurement.tobs),func.avg(Measurement.tobs)]
+    if not end:
+        results = session.query(*sel).filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+    results = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+
+
+
+# run flask app in command line:
 # export FLASK_APP=app.py
 # flask run
